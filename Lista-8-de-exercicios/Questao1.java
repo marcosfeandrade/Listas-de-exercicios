f39cbe49d93342a6fc34cdbaefe28b033bc30cfc
marcosfandrade <marcos.fellipe2211@gmail.com>
2020-10-17 11:35:39 -0300

    corrigindo a questão 1, lista 8

--- Lista-8-de-exercicios/Questao1.java
+++ Lista-8-de-exercicios/Questao1.java
@@ -6,67 +6,44 @@ import java.util.Scanner;
 public class Questao1 {
 
     public static void main(String[] args) {
-        char gab[] = new char[100];
-        char resp[][] = new char[50][100]; //alunos = linhas, resp = colunas
-        String nome[] = new String[50];
-        int certa[] = new int[50];
-        fillsGab(gab);
-        fillsName(nome);
-        fillsResp(resp, nome);
-        correct(gab, resp, certa);
-        apRep(certa, nome);
+        int[][] vectM = new int[4][4];
+        int aux;
+        System.out.println("Preencha a matriz");
+        fillsVect(vectM);
+        aux = symmetrical(vectM);
+        if (aux == 0) {
+            System.out.println("A matriz é simétrica!");
+        } else {
+            System.err.println("A matriz não é simétrica!");
+        }
     }
 
-    public static void fillsGab(char[] gab) {
+    public static void fillsVect(int[][] vectM) {
         Scanner in = new Scanner(System.in);
-        for (int i = 0; i < gab.length; i++) {
-            System.out.println("Questao " + (i + 1) + ": ");
-            gab[i] = in.next().charAt(0);
-            gab[i] = Character.toUpperCase(gab[i]);
-            while (gab[i] != 'A' && gab[i] != 'B' && gab[i] != 'C' && gab[i] != 'D' && gab[i] != 'E') {
-                System.err.println("Resposta invalida, apenas (A, B, C, D ou E)");
-                System.out.println("Questao " + (i + 1));
-                gab[i] = in.next().charAt(0);
-                gab[i] = Character.toUpperCase(gab[i]);
+        for (int i = 0; i < vectM.length; i++) {
+            for (int j = 0; j < vectM[i].length; j++) {
+                System.out.println("(" + (i + 1) + ", " + (j + 1) + ")");
+                vectM[i][j] = in.nextInt();
             }
         }
     }
 
-    public static void fillsName(String[] nome) {
-        Scanner in = new Scanner(System.in);
-        for (int i = 0; i < nome.length; i++) {
-            nome[i] = in.nextLine();
-        }
-    }
-
-    public static void fillsResp(char[][] resp, String[] nome) {
-        Scanner in = new Scanner(System.in);
-        for (int a = 0; a < resp.length; a++) {
-            for (int r = 0; r < resp[a].length; r++) {
-                System.out.println("Aluno " + nome[a] + " insira a resposta da questao " + r + ": ");
-                resp[a][r] = in.next().charAt(0);
-            }
-        }
-    }
-
-    public static void correct(char[] gab, char[][] resp, int[] certa) {
-        for (int l = 0; l < resp.length; l++) {
-            for (int c = 0; c < gab.length; c++) {
-                if (resp[l][c] == gab[c]) {
-                    certa[l]++;
+    public static int symmetrical(int[][] vectM) {
+        boolean sym = true;
+        for (int i = 0; i < vectM.length; i++) {
+            for (int j = 0; j < vectM[i].length; j++) {
+                if (vectM.length != vectM[i].length) {
+                    sym = false;
+                }
+                if (vectM[i][j] != vectM[j][i]) {
+                    sym = false;
                 }
             }
         }
-    }
-    
-    public static void apRep(int[] certa, String[] nome){
-        for(int i = 0; i < certa.length; i++){
-            if(certa[i] > 70){
-                System.out.println("O aluno " + nome[i] + " esta aprovado!");
-            }
-            else{
-                System.out.println("O aluno " + nome[i] + " esta reprovado");
-            }
+        if (sym == true) {
+            return 0;
         }
+        return -1;
     }
+
 }
